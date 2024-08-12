d91c1d429116b86fa5c44575a07aeb13c3071780
teo1404 <teooviedo@gmail.com>
2024-08-12 18:28:08 -0300

    todo nuevo

--- data/detalles.js
+++ data/detalles.js
@@ -1,47 +1,76 @@
-function setArticleView(){
-    return /*html*/`
-    <header class="article">
-      <span class="icon" onclick='setView("home")'>arrow_back_ios</span>
-      <div class="product-info">
-        <h2></h2>
-        <div class="price">
-          <h3>$30.00</h3>
-          <h5>$36.00</h5>
-        </div>
-      </div>
-      <div class="img-container">
-        <img src="img/img-00.jpg" alt="">
-      </div>
-      <div class="slider">
-        <span></span>
-        <span class="selected"></span>
-        <span></span>
-      </div>
-    </header>
-    <main>
-      
-    </main>`
+let color="white";
+let size="l";
+let addToCart=async (articleToAdd)=>{
+  let article={};
+  let data=await request();
+  data.forEach(e => e.id==articleToAdd ? article=e : 0);
+  article.color=color;
+  article.size=size;
+  localStorage.setItem("cart",
+    localStorage.getItem("cart") ? localStorage.getItem("cart").concat(","+JSON.stringify(article)) : JSON.stringify(article)
+  );
+  setView("cart");
+}
+let setSelectedOption=(ubic,idSelected)=>{
+  document.querySelectorAll(`${ubic}`).forEach(e => {
+    e.id == idSelected ? e.classList.add("selected") : e.classList.remove("selected")
+  })
+  if(ubic=="#color-list span"){
+    color=idSelected;
+  }else if(ubic=="#size-list span"){
+    size=idSelected;
   }
-  let setArticleData=(articleSelected)=>{
-    fetch('data/jsons/articulos.json')
-    .then(response => {return response.json()})
-    .then(data => {
-      data.map(e => {e.id == articleSelected ? document.querySelector("header").innerHTML=(/*html*/`
-        <span class="icon" onclick='setView("home")'>arrow_back_ios</span>
-        <div class="product-info">
-          <h2>${e.name}</h2>
-          <div class="price">
-            <h3>$${e.price.now}</h3>
-            <h5>$${e.price.then}</h5>
-          </div>
-        </div>
+}
+function setArticleView(data,articleSelected){
+  let article={};
+  data.forEach(e => e.id==articleSelected ? article=e : 0)
+  return /*html*/`
+  <section id="heading">
+    <div class="icon-container">
+      <span class="icon" onclick='setView("home")'>arrow_back</span>
+      <span class="icon" onclick='setView("cart")'>shopping_cart</span>
+    </div>
+    <scroll-container>
+      <scroll-page id="page-1">
         <div class="img-container">
-          <img src="${e.img.src}" alt="${e.img.alt}">
+          <img src="${article.img.src}" alt="${article.img.alt}">
         </div>
-        <div class="slider">
-          <span></span>
-          <span class="selected"></span>
-          <span></span>
-        </div>`) : 0});
-    })
-  }
+      </scroll-page>
+    </scroll-container>
+    <nav>
+      <a id="a1" href="#page-1" onClick="setSelectedOption('nav a',id)" class="selected"></a>
+      <a id="a2" href="#page-2" onClick="setSelectedOption('nav a',id)"></a>
+      <a id="a3" href="#page-3" onClick="setSelectedOption('nav a',id)"></a>
+    </nav>
+  </section>
+  <section class="product-info">
+    <div>
+      <h1>${article.name}</h1>
+      <div class="price">
+        <h2>$${article.price.now}</h2>
+        <h4>$${article.price.then}</h4>
+      </div>
+    </div>
+    <div>
+      <h5>Colour</h5>
+      <div id="color-list">
+        <span id="white" onClick="setSelectedOption('#color-list span',id)" class="selected"><svg></svg></span>
+        <span id="gray" onClick="setSelectedOption('#color-list span',id)"><svg></svg></span>
+        <span id="black" onClick="setSelectedOption('#color-list span',id)"><svg></svg></span>
+        <span id="blue" onClick="setSelectedOption('#color-list span',id)"><svg></svg></span>
+      </div>
+    </div>
+    <div>
+      <h5>Size</h5>
+      <div id="size-list">
+        <span id="s" onClick="setSelectedOption('#size-list span',id)">S</span>
+        <span id="m" onClick="setSelectedOption('#size-list span',id)">M</span>
+        <span id="l" onClick="setSelectedOption('#size-list span',id)" class="selected">L</span>
+        <span id="xl" onClick="setSelectedOption('#size-list span',id)">XL</span>
+        <span id="xxl" onClick="setSelectedOption('#size-list span',id)">XXL</span>
+      </div>
+    </div>
+    <button id="end" onClick="addToCart(${article.id})">Add to cart</button>
+    <!--<h5>Comments</h5>-->
+  </section>`
+}
--- data/home.js
+++ data/home.js
@@ -1,117 +1,114 @@
-function setHomeView(){
-    return /*html*/`
-    <header>
+let showAll=()=>{
+  document.querySelector(".article-scroll").classList.toggle(".showAll")
+}
+let setSelectedImage=(idSelected)=>{
+  document.querySelectorAll("nav a").forEach(e => {
+    e.id == idSelected ? e.classList.add("selected") : e.classList.remove("selected")
+  })
+}
+function setHeaderScroll(data){
+  let result="";
+  data.forEach(e=>{
+    if(e.id<3) {
+      result+=/*html*/`<scroll-page id="${e.id}" onClick='setView("article",id)'>
+        <div class="product-info">
+          <h5>Recommendations</h5>
+          <h2>${e.name}</h2>
+          <div class="price">
+            <h3>$${e.price.now}</h4>
+            <h5>$${e.price.then}</h5>
+          </div>
+        </div>
+        <div class="img-container">
+          <img src="${e.img.src}" alt="${e.img.alt}">
+        </div>
+      </scroll-page>`
+    }
+  })
+  return result;
+}
+function setNav(data){
+  let result="";
+  data.forEach(e=>{
+    if(e.id<3) {
+      result+=/*html*/`<a id="a${e.id}" href="#${e.id}" ${e.id==0?'class="selected"':0} onClick="setSelectedImage(id)"></a>`
+    }
+  });
+  return result;
+}
+function setArticleScroll(data){
+  let result="";
+  data.forEach(e=>{
+    if(e.id>2) {
+      result+=/*html*/`
+      <article id="${e.id}" onClick='setView("article",id)'>
+        <div class="img-container">
+            <img src="${e.img.src}" alt="${e.img.alt}">
+        </div>
+        <div class="article-info">
+            <h5>${e.name}</h5>
+            <div class="price">
+                <h4>$${e.price.now}</h4>
+                <h5>$${e.price.then}</h5>
+            </div>
+        </div>
+      </article>`
+    }
+  });
+  return result;
+}
+function setHomeView(data){
+  return/*html*/`
+    <section id="heading">
       <div class="search">
         <input class="icon" type="submit" value="search">
         <input type="search" placeholder="What are you looking for?">
       </div>
-      <div class="product-info">
-        <h5>Recommendations</h5>
-        <h2>Women Blue Denim</h2>
-        <div class="price">
-          <h3>$30.00</h3>
-          <h5>$36.00</h5>
-        </div>
+      <scroll-container>
+        ${setHeaderScroll(data)}
+      </scroll-container>
+      <nav>
+        ${setNav(data)}
+      </nav>
+    </section>
+    <section id="Trending" class="article-list">
+      <div class="title">
+        <h4>Trending</h4>
+        <h6 onClick='showAll()'>Show all</h6>
       </div>
-      <div class="img-container">
-        <img src="img/img-00.jpg" alt="">
+      <div class="article-scroll">
+        ${setArticleScroll(data)}
       </div>
-      <div class="slider">
-        <span></span>
-        <span class="selected"></span>
-        <span></span>
+    </section>
+    <section class="Categories">
+      <div class="title">
+        <h4>Categories</h4>
+        <h6>Show all</h6>
       </div>
-    </header>
-    <main>
-      <section id="Trending" class="article-list">
-        <div class="title">
-          <h4>Trending</h4>
-          <h6>Show all</h6>
-        </div>
-        <div class="article-scroll"></div>
-      </section>
-      <section class="Categories">
-        <div class="title">
-          <h4>Categories</h4>
-          <h6>Show all</h6>
-        </div>
-        <div class="categories-list">
-          <article>
-            <div class="icon">woman</div>
-            <div class="name">Woman<h6>(728 items)</h6></div>
-            <div class="icon">chevron_right</div>
-          </article>
-          <article>
-            <div class="icon">man</div>
-            <div class="name">Man<h6>(536 items)</h6></div>
-            <div class="icon">chevron_right</div>
-          </article>
-        </div>
-      </section>
-      <section class="article-list">
-        <div class="title">
-          <h4>Best Seller</h4>
-          <h6>Show all</h6>
-        </div>
-        <div class="article-scroll">
-          <article>
-            <div class="img-container">
-              <img src="img/img-01.jpg" alt="">
-            </div>
-            <div class="article-info">
-              <h5>Buzo Capo</h5>
-              <div class="price">
-                <h4>$35.99</h4>
-                <h5>$39.99</h5>
-              </div>
-            </div>
-          </article>
-          <article>
-            <div class="img-container">
-              <img src="img/img-02.jpg" alt="">
-            </div>
-            <div class="article-info">
-              <h5>Russ Shirt</h5>
-              <div class="price">
-                <h4>$19.99</h4>
-                <h5>$25.99</h5>
-              </div>
-            </div>
-          </article>
-          <article>
-            <div class="img-container">
-              <img src="img/img-03.jpg" alt="">
-            </div>
-            <div class="article-info">
-              <h5>Greek Shirt</h5>
-              <div class="price">
-                <h4>$18.99</h4>
-                <h5>$24.99</h5>
-              </div>
-            </div>
-          </article>
-        </div>
-      </section>
-    </main>`
-  }
-  function setArticleList(){
-    fetch('data/jsons/articulos.json')
-    .then(response => {return response.json()})
-    .then(data => {
-      data.forEach(e=>{
-        document.querySelector("#Trending .article-scroll").innerHTML +=/*html*/`
-        <article id="${e.id}" onClick='setView("article",id)'>
+      <div class="categories-list">
+        <article>
+          <!--<div class="icon">woman</div>-->
           <div class="img-container">
-              <img src="${e.img.src}" alt="${e.img.alt}">
+            <img src="img/icon-m.png" alt="">
           </div>
-          <div class="article-info">
-              <h5>${e.name}</h5>
-              <div class="price">
-                  <h4>$${e.price.now}</h4>
-                  <h5>$${e.price.then}</h5>
-              </div>
+          <div class="name">Woman<h6>(728 items)</h6></div>
+          <div class="icon">chevron_right</div>
+        </article>
+        <article>
+          <!--<div class="icon">man</div>-->
+          <div class="img-container">
+            <img src="img/icon-w.png" alt="">
           </div>
-      </article>`
-      });
-    })
-  }
+          <div class="name">Man<h6>(536 items)</h6></div>
+          <div class="icon">chevron_right</div>
+        </article>
+      </div>
+    </section>
+    <section class="article-list">
+      <div class="title">
+        <h4>Best Seller</h4>
+        <h6>Show all</h6>
+      </div>
+      <div class="article-scroll"></div>
+    </section>`
+}
